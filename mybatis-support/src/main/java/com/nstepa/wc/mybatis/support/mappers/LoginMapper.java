ac8f5ab5031c75d6e9de474c9e09b3aa52d896b6
zhangwei <zhangwei2017@unipus.cn>
2018-12-05 09:17:05 +0800

    login登录及用户mapper

---
+++ mybatis-support/src/main/java/com/nstepa/wc/mybatis/support/mappers/LoginMapper.java
@@ -0,0 +1,17 @@
+package com.nstepa.wc.mybatis.support.mappers;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.nstepa.wc.beans.Login;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author henry
+ * @since 2018-09-06
+ */
+public interface LoginMapper extends BaseMapper<Login> {
+
+}
---
+++ mybatis-support/src/main/java/com/nstepa/wc/mybatis/support/mappers/UserMapper.java
@@ -0,0 +1,16 @@
+package com.nstepa.wc.mybatis.support.mappers;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.nstepa.wc.beans.User;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author henry
+ * @since 2018-09-06
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
